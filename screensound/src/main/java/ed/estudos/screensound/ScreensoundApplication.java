3050ba49c29338fefafde5d07472444aaafc55b9
Ladeiraalexandre <ladeiraalexandre@gmail.com>
2024-08-10 00:33:42 -0300

    Estrutura básica projeto e configuração do banco

--- screensound/src/main/java/ed/estudos/screensound/ScreensoundApplication.java
+++ screensound/src/main/java/ed/estudos/screensound/ScreensoundApplication.java
@@ -1,13 +1,23 @@
 package ed.estudos.screensound;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ed.estudos.screensound.principal.Principal;
+
 @SpringBootApplication
-public class ScreensoundApplication {
+public class ScreensoundApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreensoundApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+		
 	}
 
 }
